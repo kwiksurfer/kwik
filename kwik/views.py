0862aeffddbc0024a7c094658cd14c2e6b76a665
Olusegun OJO <ojosamuelolusegun@gmail.com>
2015-01-17 19:01:40 +0100

    checking for path. Sorry about all this trial and error thing

--- kwik/views.py
+++ kwik/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+import os
 import datetime
 
+this_path = os.getcwd()
 
 def current_time(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body><p>It is now %s.</p><p>Directory: %s</body></html>" % (now, this_path)
     return HttpResponse(html)
 
 def tester(request):
